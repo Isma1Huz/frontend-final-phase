70ec35d5c44e4551fded50fe4e2439c590259296
phillip <phillip.lloyd@student.moringaschool.com>
2023-11-01 14:23:07 +0300

    corrections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import RecipePage from './Pages/RecipePage/RecipePage';
 import RecipeRoom from './RecipeRoom';
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 
 function App() {
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,18 +4,18 @@ import './Header.css';
 function Header() {
   return (
     <header className="app-header">
-      <div className="top-left">
-        <div className="logo">
-          <img src="logo.png" alt="Logo" />
+      <div className="header-left">
+        <div className="logo-container">
+          <img src="logo.png" alt="Logo" className="logo" />
         </div>
-        <div className="search-bar">
-          <input type="text" placeholder="Search for recipes" />
+        <div className="search-container">
+          <input type="text" placeholder="Search for recipes" className="search" />
         </div>
       </div>
-      <div className="top-right">
-        <button className="recipe">Recipes</button>
-        <button className="register">Register</button>
-        <button className="login">Login</button>
+      <div className="header-right">
+        <button className="header-button recipe">Recipes</button>
+        <button className="header-button register">Register</button>
+        <button className="header-button login">Login</button>
       </div>
     </header>
   );
