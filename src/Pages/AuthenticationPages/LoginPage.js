dec888406877f0d3ed346b7288370bfc60c48da9
ubah feisal <feisalubah1@gmail.com>
2023-11-01 11:40:44 +0300

    implemoented sign in and sign up forms

--- src/Pages/AuthenticationPages/LoginPage.js
+++ src/Pages/AuthenticationPages/LoginPage.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../login.css'
+import loginImage from './WhatsApp_Image_2023-10-26_at_12.17.35-removebg-preview.png';
 
-function LoginPage() {
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert(`Email: ${email}\nPassword: ${password}`);
+  };
+
   return (
-    <div>
-        <h1>Login Page</h1>
-      
+    <div className='form-cl'>
+      <img className='img-cl' src={loginImage} alt="login" width="150" /> 
+      <div className='containerl'>   
+        <h2 className='hl'>Sign In</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <input className='login-inp'
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div>
+            <input className='login-inp'
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button className='login-btn' type="submit">Login</button>
+          <p className='pl3'> Forgot your password</p>
+          <p className='pl1'> Don't have an account?</p>
+          <p className='pl2'> Create now</p>
+        </form>
+      </div>  
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
