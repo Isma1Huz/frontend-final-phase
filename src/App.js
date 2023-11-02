67e37735cbc813abfa323c4fdecc1bf801fc4834
john <johnmaluki58@gmail.com>
2023-11-02 21:27:52 +0300

    Make header and footer responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
-import RecipeRoom from './RecipeRoom';
-
+import "./App.css";
+import RecipeRoom from "./RecipeRoom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <RecipeRoom />
-  
+      <Header />
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,4 @@
-
-// import React from 'react';
-// import './Footer.css'
+import "./Footer.css";
 
 // const footerMenus = [
 //   {
@@ -36,7 +34,64 @@
 //       ))}
 //     </div>
 //   );
-//   // 
+//   //
 // }
 
 // export default Footer;
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-top-cover">
+        <div className="log-copyright">
+          <h6 className="logo">
+            Re<span>st</span>Room
+          </h6>
+          <h6 className="copyright">All Rights Reserved</h6>
+          <div className="social-icons">
+            <i className="fa fa-facebook" aria-hidden="true"></i>
+            <i className="fa fa-instagram" aria-hidden="true"></i>
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+            <i className="fa fa-pinterest-p" aria-hidden="true"></i>
+            <i className="fa fa-youtube-play" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div className="fresh-recipe-news">
+          <div className="fresh-recipes">
+            <h3 className="footer-items-title">Fresh Recipe</h3>
+            <ul className="footer-items">
+              <li>Recipes</li>
+              <li>Winter salads</li>
+              <li>Organic chicken</li>
+              <li>Beef and Mutton</li>
+              <li>Flavoured Milk</li>
+            </ul>
+          </div>
+          <div className="in-news">
+            <h3 className="footer-items-title">In News</h3>
+            <ul className="footer-items">
+              <li>Our Blogs</li>
+              <li>Contests/Sweepsatkes</li>
+              <li>Video</li>
+              <li>New Releases</li>
+              <li>Newsletters</li>
+            </ul>
+          </div>
+        </div>
+        <div className="about-us">
+          <h3 className="footer-items-title">About Us</h3>
+          <ul className="footer-items">
+            <li>FAQ</li>
+            <li>Our Board</li>
+            <li>Our Staff</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
