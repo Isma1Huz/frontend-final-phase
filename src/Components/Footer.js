f026208a2ba1d7cc38601f0d6bf11eaa40dcb399
john <johnmaluki58@gmail.com>
2023-11-02 21:32:33 +0300

    Remove comments

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,44 +1,4 @@
 import "./Footer.css";
-
-// const footerMenus = [
-//   {
-//     title: 'Fresh Recipe',
-//     items: ['Home', 'News', 'About Us', 'Recipes', 'Our Blogs'],
-//   },
-//   {
-//     title: 'FAQ',
-//     items: ['Winter Salads', 'Contests/Sweepsatkes', 'Our Board', 'Organic Chicken', 'Video'],
-//   },
-//   {
-//     title: 'Our Staff',
-//     items: ['Beef and Mutton', 'New Releases', 'Contact Us', 'Flavored Milk', 'Newsletters'],
-//   },
-//   {
-//     title: 'Our Staff',
-//     items: ['Beef and Mutton', 'New Releases', 'Contact Us', 'Flavored Milk', 'Newsletters'],
-//   },
-// ];
-
-// function Footer() {
-//   return (
-//     <div className="footer">
-//       {footerMenus.map((menu, index) => (
-//         <ul key={index}>
-//           <li style={{ fontWeight: 'bold', color: 'white' , listStyleType: 'none'}}>{menu.title}</li>
-//           {menu.items.map((item, itemIndex) => (
-//             <li key={itemIndex}>
-//               <a href="#">{item}</a>
-//             </li>
-//           ))}
-//         </ul>
-//       ))}
-//     </div>
-//   );
-//   //
-// }
-
-// export default Footer;
-
 import React from "react";
 
 const Footer = () => {
